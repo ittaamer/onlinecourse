0bb1b31c604409953dcc1ce9784062ee47ad1826
ittaamer <itamerliana82@gmail.com>
2021-11-04 18:36:20 +0700

    commit pertama

---
+++ admin/includes/header.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(0);
+?>
+
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                 <a class="navbar-brand" href="#" style="color:#fff; font-size:24px;4px; line-height:24px; ">
+
+                 <img src="https://objectstorage.ap-sydney-1.oraclecloud.com/n/sdn9aicvpqxg/b/bucket-uts/o/uts-bucketundraw_engineering_team_u99p.png" style="width:130px" alt="">
+              
+                </a>
+
+            </div>
+
+            <div class="left-div">
+                <i class="fa fa-user-plus login-icon" ></i>
+        </div>
+            </div>
+        </div>
